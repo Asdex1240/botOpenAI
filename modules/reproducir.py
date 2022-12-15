02508b0e4aca458cc8482a2dadcb03e0154b67f4
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-12-15 13:42:31 -0600

    Borramos todo

--- modules/reproducir.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def reproducir(archivo):
-    pygame.mixer.init()
-    pygame.mixer.music.load(archivo)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
--- modules/voz.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-from modules.reproducir import reproducir
-def convertirVoz(respuesta):
-    tts = gTTS(respuesta, lang="es")
-    ruta = "./media/respuesta.mp3"
-    tts.save(ruta)
-    reproducir(ruta)
