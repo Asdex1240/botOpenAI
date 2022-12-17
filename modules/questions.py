635c96ac3008ed160475b1423c1c01b47d00dc40
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-12-17 14:30:08 -0600

    Quité linea innecesarias

--- modules/questions.py
+++ modules/questions.py
@@ -2,15 +2,12 @@ import openai
 from modules.reconocimiento import voice
 from modules.voz import convertirVoz
 def openaiQ():
+    
     convertirVoz("¿Qué quieres preguntar?")
-    question =  voice()
 
-    response = openai.Completion.create(
+    return openai.Completion.create(
         model="text-davinci-003",
-        prompt=question,
+        prompt=voice(),
         max_tokens=128,
         temperature=1
-    )
-    respuesta = response.choices[0].text
-    print(respuesta)
-    return respuesta
+    ).choices[0].text
